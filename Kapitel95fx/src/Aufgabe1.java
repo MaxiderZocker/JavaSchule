168dcc6f916ae98a757161d8339d63318406f555
Max <87779396+MaxiderZocker@users.noreply.github.com>
2023-11-28 14:34:15 +0100

    Finish after Lesson

--- Kapitel95fx/src/Aufgabe1.java
+++ Kapitel95fx/src/Aufgabe1.java
@@ -3,36 +3,68 @@ import javafx.stage.*;
 import javafx.scene.*;  
 import javafx.scene.shape.*; 
 import javafx.scene.paint.*;  
-
-import java.util.Random;
-
+ 
 public class Aufgabe1 extends Application
 { 
-  double sceneWidth=400, sceneHeight= 300;  
-  double centerX=sceneWidth/2, centerY=sceneHeight/2;
-
-  private void fillGroup( Group gr, double scale )
-  {
-    double width=sceneWidth*scale, height=sceneHeight*scale;
+  
+  void addStar( Group root, Color color, double centerX, double centerY, double size )
+  { 
+    double startX, endX, startY, endY;
     
-    // Add one rectangle to the Group
-    Rectangle rect = new Rectangle(  centerX-width/2, centerY-height/2, width, height );
-    rect.setStrokeWidth( 2.0 );
-    rect.setStroke( Color.RED );
-    rect.setFill( Color.TRANSPARENT );
-    gr.getChildren().add( rect );  
+    // horizontal line
+    startX = centerX - size/2;
+    endX   = centerX + size/2;
+    Line line = new Line( startX, centerY, endX, centerY );
+    line.setStrokeWidth( 2.0 );
+    line.setStroke( color );
+    
+    // circles at both ends
+    Circle left  = new Circle( centerX-size/2, centerY, size*0.1 );
+    Circle right = new Circle( centerX+size/2, centerY, size*0.1 );
+    
+    // make a Group of all three Shapes
+    Group  grp   = new Group( line, left, right ); 
+    root.getChildren().add( grp );   // add the group to the scene graph
+    
+    // upward slanting line
+    line  = new Line( startX, centerY, endX, centerY );
+    line.setStrokeWidth( 2.0 );
+    line.setStroke( color );
 
-    // If there are more rectangles, add them to the Group
-    if ( scale>=0.2 )
-      fillGroup( gr, scale-0.1 );      
+    // circles at both ends
+    left  = new Circle( centerX-size/2, centerY, size*0.1 );
+    right = new Circle( centerX+size/2, centerY, size*0.1 );
+
+    // make a Group of all three Shapes
+    grp   = new Group( line, left, right );   
+    grp.setRotate( -60.0 );
+    root.getChildren().add( grp );   // add the group to the scene graph
+    
+    // downward slanting line
+    line  = new Line( startX, centerY, endX, centerY );
+    line.setStrokeWidth( 2.0 );
+    line.setStroke( color );
+    
+    // circles at both ends
+    left  = new Circle( centerX-size/2, centerY, size*0.1 );
+    right = new Circle( centerX+size/2, centerY, size*0.1 );
+
+    // make a Group of all three Shapes
+    grp   = new Group( line, left, right );   
+    grp.setRotate( +60.0 );
+    root.getChildren().add( grp );     // add the group to the scene graph
+    
   }
-
+  
   public void start(Stage stage) 
   { 
+    double sceneWidth=400, sceneHeight= 300;  
     Group  root = new Group( );   
-    fillGroup( root, 0.9 );
-    Scene scene = new Scene(root, sceneWidth, sceneHeight, Color.GHOSTWHITE ); 
-    stage.setTitle("Recursive Rectangles"); 
+  
+    addStar( root, Color.BLUE, sceneWidth/2, sceneHeight/2, sceneWidth*0.6 );
+    
+    Scene scene = new Scene(root, sceneWidth, sceneHeight, Color.SNOW ); 
+    stage.setTitle("One Star"); 
     stage.setScene(scene); 
     stage.show(); 
   }      
