4606c2f5a48e239e5dadede8eabe7351e6a728c0
Max <87779396+MaxiderZocker@users.noreply.github.com>
2023-12-12 14:26:42 +0100

    end of lesson

--- Kapitel104/src/Aufgabe3.java
+++ Kapitel104/src/Aufgabe3.java
@@ -5,16 +5,19 @@ class Aufgabe3
 	public static void main ( String[] args ) throws IOException
 	{
 		String fileName = "Aufgabe3.dat" ;
-		Scanner scan = new Scanner ( "Aufgabe3.txt" );
+		Scanner scan = new Scanner ( new File ("Aufgabe3.txt") );
 		int input;
 
-		DataOutputStream dataOut = new DataOutputStream(new BufferedOutputStream(new FileOutputStream( fileName  ) ) );
+		PrintWriter dataOut = new PrintWriter(fileName, "UTF-8" );
 		
 		
 		//for(int i=0; i<=4; i++) {
+		while (scan.hasNextLine()) {
 			input = Integer.parseInt(scan.nextLine());
-		
-			dataOut.writeInt(input);	
+			
+			dataOut.println(input);
+		}
+
 		//}
 
 		dataOut.close();
