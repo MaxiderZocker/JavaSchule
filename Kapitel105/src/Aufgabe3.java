3c4d7018ccfe7e1d4e7b621d83f93a2f1e7df8bf
Max <87779396+MaxiderZocker@users.noreply.github.com>
2024-01-09 14:24:58 +0100

    End of Lesson

---
+++ Kapitel105/src/Aufgabe3.java
@@ -0,0 +1,34 @@
+import java.io.*;
+
+class ReadIntEOF {
+    public static void main(String[] args) {
+        String fileName = "ints.dat";
+        long sum = 0;
+        DataInputStream instr = null;
+
+        try {
+            instr = new DataInputStream(
+                    new BufferedInputStream(
+                            new FileInputStream(fileName)));
+
+            try {
+                while (true)
+                    sum += instr.readInt();
+            } catch (EOFException eof) {
+                System.out.println("The sum is: " + sum);
+            } catch (IOException iox) {
+                System.out.println("Problems reading " + fileName);
+            }
+        } catch (IOException iox) {
+            System.out.println("I/O Problems with " + fileName);
+        } finally {
+            try {
+                if (instr != null) {
+                    instr.close();
+                }
+            } catch (IOException closeException) {
+                System.out.println("Error closing file: " + closeException.getMessage());
+            }
+        }
+    }
+}
